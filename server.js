1a094729970d7b5897f123f6040244d4ef303ebb
Dapo <adedap09@gmail.com>
2017-11-11 20:49:42 +0100

    Added the Projects route

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        projectMessage: 'Watch out for projects here'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to fulfill this request'
